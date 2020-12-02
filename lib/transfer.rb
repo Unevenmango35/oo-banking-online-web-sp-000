1f9ba93f9d87da818d2d720ea83f9621ff14c612
Rosie Choi <rosiechoimanso@gmail.com>
2020-12-02 04:52:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -6,5 +6,14 @@ class Transfer
     @receiver = receiver
     @amount = amount
     @status = status
+  end
+
+  def valid?(sender,receiver)
+    if sender.status && receiver.status
+      then true
+    else false
+    end
+  end
+  
 
 end
