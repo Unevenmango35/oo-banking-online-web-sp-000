a3321a890d79b399deaebb74a74ac6eeb70a4a31
Rosie Choi <rosiechoimanso@gmail.com>
2020-12-02 05:11:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   def execute_transaction
     if !self.valid? && @sender.balance <= @amount
      @status = "rejected"
-     @status = "Transaction rejected. Please check your account balance."
+     puts "Transaction rejected. Please check your account balance."
     elsif @status == "pending"
       BankAccount.all.map do |person|
         if person == @sender
