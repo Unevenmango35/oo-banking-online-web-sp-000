1d56851db5714c020eaecc53ec9bba864fb85a28
Rosie Choi <rosiechoimanso@gmail.com>
2020-12-02 04:15:53 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,10 @@
 class BankAccount
 attr_accessor :name, :balance, :status
 
-def initialize(name, balance=1000)
+def initialize(name, balance=1000, status="open")
   @name=name
   @balance = balance
-  @status = "open"
+  @status = status
 end
 
 def deposit(amt)
