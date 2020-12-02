32d2497082c47351bc5019bf2018b1cce02416b7
Rosie Choi <rosiechoimanso@gmail.com>
2020-12-02 05:00:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
-  def initialize(status="pending")
+  def initialize(sender, receiver, amount, status="pending")
     @sender = sender
     @receiver = receiver
     @amount = amount
